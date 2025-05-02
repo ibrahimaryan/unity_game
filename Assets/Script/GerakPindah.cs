75959e6e9f19839aba5e57ef8c3a5855efdac2b7
Miftachussurur <miftachussurur1993@gmail.com>
2025-05-02 08:50:38 +0700

    game 3 belom jadi, baru awal banget

---
+++ Assets/Script/GerakPindah.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GerakPindah : MonoBehaviour
+{
+
+    //float speed = 3f;
+   // public Sprite[] sprites;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //int index = Random.Range(0, sprite.Length);
+        //gameObject.GetComponent<SpriteRenderer>().sprite = sprites[index];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
