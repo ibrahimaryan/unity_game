2c47b6a2a76b52545a726803a2c043d5a6da2160
Miftachussurur <miftachussurur1993@gmail.com>
2025-05-02 10:17:48 +0700

    game 3 contoh (masih error)

--- Assets/Script/GerakPindah.cs
+++ Assets/Script/GerakPindah.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class GerakPindah : MonoBehaviour
 {
 
-    //float speed = 3f;
-   // public Sprite[] sprites;
+    float speed = 3f;
+    public Sprite[] sprites;
     // Start is called before the first frame update
     void Start()
     {
-        //int index = Random.Range(0, sprite.Length);
-        //gameObject.GetComponent<SpriteRenderer>().sprite = sprites[index];
+        int index = Random.Range(0, sprites.Length);
+        gameObject.GetComponent<SpriteRenderer>().sprite = sprites[index];
     }
 
     // Update is called once per frame
@@ -19,4 +19,24 @@ public class GerakPindah : MonoBehaviour
     {
         
     }
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    private float firstY;
+    void OnMouseDown()  
+        {
+            firstY = transform.position.y;
+            screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+            offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        }
+    void OnMouseDrag()
+        {
+            Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+            transform.position = curPosition;
+        }
+    private void OnMouseUp()
+        {
+            transform.position = new Vector3(transform.position.x,firstY, transform.position.z);
+        }
+
 }
